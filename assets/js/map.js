bf7b41a25bb8dbba3c8f0f2e5ece821c71b80d30
Antonin <antonin@litschgy.fr>
2024-03-31 13:46:00 +0200

    feat: updating map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,36 +1,38 @@
 new svgMap({
-    targetElementID: 'svgMap',
-    noDataText: 'Not visited yet',
-    mouseWheelZoomEnabled : true,
-    mouseWheelZoomWithKey : true,
-    showZoomReset : true,
-    colorMax: '#57c9e0',
+  targetElementID: "svgMap",
+  noDataText: "Not visited yet",
+  mouseWheelZoomEnabled: true,
+  mouseWheelZoomWithKey: true,
+  showZoomReset: true,
+  colorMax: "#57c9e0",
+  data: {
     data: {
-      data: {
-        visited: {
-          name: '',
-          format: 'Visited',
-          thousandSeparator: ',',
-          thresholdMax: 2030,
-          thresholdMin: 0
-        }
+      visited: {
+        name: "",
+        format: "Visited",
+        thousandSeparator: ",",
       },
-      applyData: 'visited',
-      values: {
-        FR: { visited: true},
-        DE: { visited: true},
-        GB: { visited: true},
-        SE: { visited: true},
-        CH: { visited: true},
-        TR: { visited: true},
-        ES: { visited: true},
-        PT: { visited: true},
-        HR: { visited: true},
-        EC: { visited: true},
-        NZ: { visited: true},
-        AU: { visited: true}
-        
-        // ...
-      }
-    }
-  });
+      date: {
+        name: "In",
+      },
+    },
+    applyData: "visited",
+    values: {
+      FR: { visited: true, date: 2001, color: "green" },
+      DE: { visited: true, date: "Many times" },
+      GB: { visited: true, date: 2016 },
+      SE: { visited: true, date: 2022 },
+      CH: { visited: true, date: "Many times" },
+      TR: { visited: true, date: [2015, 2019] },
+      ES: { visited: true, date: [2021, 2022] },
+      PT: { visited: true, date: 2018 },
+      HR: { visited: true, date: 2021 },
+      EC: { visited: true, date: 2022 },
+      NZ: { visited: true, date: 2023 },
+      AU: { visited: true, date: 2024 },
+      US: { visited: true, date: 2024 },
+
+      // ...
+    },
+  },
+});
